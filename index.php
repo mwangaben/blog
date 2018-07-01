23a59c28c055d86836767507e6608de4b17ac4af
Benedict Mwanga <mwangaben@gmail.com>
2018-07-01 06:01:02 +0300

    The bog template created via CSS Flex Model

---
+++ index.php
@@ -0,0 +1,172 @@
+<html lang="en">
+
+<head>
+
+    <!--  Meta  -->
+    <meta charset="UTF-8" />
+    <title>Flex Box</title>
+
+    <!--  Styles  -->
+    <link rel="stylesheet" href="css/app.css">
+</head>
+
+<body>
+    <div class="header">
+        <h2>MY BLOG</h2>
+        <p>Welcome to the blog of <span>unknown</span></p>
+    </div>
+
+    <div class="container">
+        <section class="main">
+            <div class="card shadow">
+                <div class="card-header">
+                    <img src="img/woods.jpg" alt="wood">
+                </div>
+                <div class="card-content">
+                    <h4>TITLE HEADING</h4>
+                    <h4>Title description, April 7, 2014</h4>
+                    <p>Mauris neque quam, fermentum ut nisl vitae, convallis maximus nisl. Sed mattis nunc id lorem euismod placerat. Vivamus porttitor magna enim, ac accumsan tortor cursus at. Phasellus sed ultricies mi non congue ullam corper. Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.
+                    </p>
+                </div>
+                <div class="card-footer">
+                    <div class="left">
+                        <div class="btn">
+                            <ul>
+                                <li>
+                                    <button>READ MORE  <span>&gt;&gt;</span></button>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="right">
+                       <ul>
+                           <li>Comments</li>
+                           <li><span class="comment_count">0</span></li>
+                       </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="card shadow">
+                <div class="card-header">
+                    <img src="img/bridge.jpg" alt="bridge">
+                </div>
+                <div class="card-content">
+                    <h2>BLOG ENTRY</h2>
+                    <h4>Title description, April 2, 2014</h4>
+                    <p>
+                        Mauris neque quam, fermentum ut nisl vitae, convallis maximus nisl. Sed mattis nunc id lorem euismod placerat. Vivamus porttitor magna enim, ac accumsan tortor cursus at. Phasellus sed ultricies mi non congue ullam corper. Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.
+                    </p>
+                </div>
+                <div class="card-footer">
+                    <div class="left">
+                        <div class="btn">
+                                <ul>
+                                    <li>
+                                        <button>READMORE >></button>
+                                    </li>
+                                </ul>
+                        </div>
+                    </div>
+                    <div class="right">
+                       <ul>
+                           <li>Comments</li>
+                           <li><span class="comment_count">2</span></li>
+                       </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <aside>
+            <div class="card shadow">
+                <div class="card-header">
+                    <img src="img/avatar_g.jpg" alt="Avatar">
+                </div>
+                <div class="card-content">
+                    <h2>My Name</h2>
+                    <p>Just me, myself and I, exploring the universe of uknownment. I have a heart of love and a interest of lorem ipsum and mauris neque quam blog. I want to share my world with you.</p>
+                </div>
+            </div>
+            <div class="card shadow">
+                <div class="card-header">
+                    <h2>Popular Posts</h2>
+                </div>
+                <div class="card-content post">
+                    <div class="content">
+                        <div class="left">
+                            <img src="img/workshop.jpg" alt="workshop">
+                        </div>
+                        <div class="right">
+                            <h4>Lorem</h4>
+                            <p>Sed mattis nunc</p>
+                        </div>
+                    </div>
+                    <div class="content">
+                        <div class="left">
+                            <img src="img/gondol.jpg" alt="workshop">
+                        </div>
+                        <div class="right">
+                            <h4>Ipsum</h4>
+                            <p>Praes tinci sed</p>
+                        </div>
+
+                    </div>
+                    <div class="content">
+                        <div class="left">
+                            <img src="img/skies.jpg" alt="workshop">
+                        </div>
+                        <div class="right">
+                            <h4>Dorum</h4>
+                            <p>Ultricies congeu</p>
+                        </div>
+                    </div>
+                    <div class="content">
+                        <div class="left">
+                            <img src="img/rock.jpg" alt="rock">
+                        </div>
+                        <div class="right">
+                            <h4>Mingsum</h4>
+                            <p>Lorem ipsum dipsum</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card shadow">
+                <div class="card-header">
+                    <h2>Tags</h2>
+                </div>
+                <div class="card-content tags">
+                    <ul>
+                        <li class="active"><a href="#">Travel</a></li>
+                        <li><a href="#">New York</a></li>
+                        <li><a href="#">London</a></li>
+                        <li><a href="#">IKEA</a></li>
+                        <li><a href="#">NORWAY</a></li>
+                        <li><a href="#">DIY</a></li>
+                        <li><a href="#">Ideas</a></li>
+                        <li><a href="#">Baby</a></li>
+                        <li><a href="#">Family</a></li>
+                        <li><a href="#">News</a></li>
+                        <li><a href="#">Clothing</a></li>
+                        <li><a href="#">Shopping</a></li>
+                        <li><a href="#">Sports</a></li>
+                        <li><a href="#">Games</a></li>
+                    </ul>
+                </div>
+            </div>
+        </aside>
+    </div>
+    <footer>
+        <div class="paginate">
+            <button class="active">
+        Previous
+      </button>
+            <button>
+        Next >>
+      </button>
+        </div>
+        <p>Powered By Umbijani</p>
+    </footer>
+
+</body>
+
+</html>
